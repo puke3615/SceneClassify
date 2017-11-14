4fb2f61ffda3845bb4d5b6ccf3aba7e8a3c3412b
zijiao <1129090915@qq.com>
2017-11-14 09:36:16 +0800

    update networks

--- client_small.py
+++ client_small.py
@@ -2,7 +2,7 @@ from keras.layers import *
 from keras.models import *
 from keras.preprocessing.image import ImageDataGenerator
 from keras.callbacks import ModelCheckpoint
-from keras.optimizers import SGD
+from keras.optimizers import *
 
 from generator import SenceDirectoryIterator
 import utils
@@ -18,12 +18,12 @@ PATH_TRAIN_JSON = os.path.join(PATH_TRAIN_BASE, 'scene_train_annotations_2017090
 PATH_VAL_IMAGES = os.path.join(PATH_VAL_BASE, 'scene_validation_images_20170908')
 PATH_VAL_JSON = os.path.join(PATH_VAL_BASE, 'scene_validation_annotations_20170908.json')
 PATH_WEIGHTS = 'params/weights.h5'
-IM_WIDTH = 64
-IM_HEIGHT = 64
+IM_WIDTH = 256
+IM_HEIGHT = 256
 BATCH_SIZE = 128
 CLASSES = 80
 EPOCH = 50
-LEARNING_RATE = 1e-1
+LEARNING_RATE = 1e-2
 
 
 def build_generator(path_image, path_json, train=True):
@@ -31,8 +31,6 @@ def build_generator(path_image, path_json, train=True):
         return float(train) and value
 
     image_generator = ImageDataGenerator(
-        samplewise_center=True,
-        samplewise_std_normalization=True,
         rescale=1. / 255,
         rotation_range=wrap(15.),
         width_shift_range=wrap(0.7),
@@ -60,22 +58,23 @@ if __name__ == '__main__':
     val_generator = build_generator(PATH_VAL_IMAGES, PATH_VAL_JSON, train=False)
 
     model = Sequential()
-    model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(IM_HEIGHT, IM_WIDTH, 3)))
+    model.add(Conv2D(8, (3, 3), activation='relu', input_shape=(IM_HEIGHT, IM_WIDTH, 3)))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(BatchNormalization())
-    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(Conv2D(16, (3, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(BatchNormalization())
-    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(Conv2D(16, (3, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(BatchNormalization())
     model.add(Flatten())
-    model.add(Dense(512, activation='relu'))
+    model.add(Dense(1024, activation='relu'))
     model.add(BatchNormalization())
     model.add(Dense(80, activation='softmax'))
 
-    sgd = SGD(lr=LEARNING_RATE, decay=1e-6, momentum=0.9, nesterov=True)
-    model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+    # optimizer = SGD(lr=LEARNING_RATE, decay=1e-6, momentum=0.9, nesterov=True)
+    optimizer = Adam(lr=LEARNING_RATE)
+    model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
     if os.path.exists(PATH_WEIGHTS):
         model.load_weights(PATH_WEIGHTS, True)
         print('Load weights.h5 successfully.')
--- test.py
+++ test.py
@@ -1,17 +1,7 @@
-import json
-import os
+import keras
 
-PATH_TRAIN_BASE = '/Users/zijiao/Desktop/ai_challenger_scene_train_20170904'
-path = os.path.join(PATH_TRAIN_BASE, 'scene_train_annotations_20170904.json')
-
-with open(path) as f:
-    a = json.load(f)
-    print(len(a))
-    print(a[0])
-    image2label = {d['image_id']: int(d['label_id']) for d in a}
-    print(type(image2label['79f993ae0858ae238b22968c5934d1ddba585ae4.jpg']))
-
-
-a = False
-
-print(float(a) and 0.1)
+model = keras.applications.VGG16(weights=None)
+# print model.summary()
+# print model.get_config()
+weights = model.get_weights()
+print weights
