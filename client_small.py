73b009506005e76ff8a71fab376a247b7e7d06be
zijiao <1129090915@qq.com>
2017-11-14 09:55:18 +0800

    add center handle

--- client_small.py
+++ client_small.py
@@ -18,12 +18,18 @@ PATH_TRAIN_JSON = os.path.join(PATH_TRAIN_BASE, 'scene_train_annotations_2017090
 PATH_VAL_IMAGES = os.path.join(PATH_VAL_BASE, 'scene_validation_images_20170908')
 PATH_VAL_JSON = os.path.join(PATH_VAL_BASE, 'scene_validation_annotations_20170908.json')
 PATH_WEIGHTS = 'params/weights.h5'
-IM_WIDTH = 256
-IM_HEIGHT = 256
+IM_WIDTH = 128
+IM_HEIGHT = 128
 BATCH_SIZE = 128
 CLASSES = 80
 EPOCH = 50
 LEARNING_RATE = 1e-2
+
+
+def preprocess(x):
+    x -= 127.5
+    x /= 127.5
+    return x
 
 
 def build_generator(path_image, path_json, train=True):
@@ -31,13 +37,15 @@ def build_generator(path_image, path_json, train=True):
         return float(train) and value
 
     image_generator = ImageDataGenerator(
-        rescale=1. / 255,
+        # rescale=1. / 255,
         rotation_range=wrap(15.),
         width_shift_range=wrap(0.7),
         height_shift_range=wrap(0.7),
         shear_range=wrap(0.2),
         zoom_range=wrap(0.2),
-        horizontal_flip=train)
+        horizontal_flip=train,
+        preprocessing_function=preprocess,
+    )
 
     return SenceDirectoryIterator(
         path_image,
@@ -58,17 +66,14 @@ if __name__ == '__main__':
     val_generator = build_generator(PATH_VAL_IMAGES, PATH_VAL_JSON, train=False)
 
     model = Sequential()
-    model.add(Conv2D(8, (3, 3), activation='relu', input_shape=(IM_HEIGHT, IM_WIDTH, 3)))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(BatchNormalization())
-    model.add(Conv2D(16, (3, 3), activation='relu'))
+    model.add(Conv2D(16, (3, 3), activation='relu', input_shape=(IM_HEIGHT, IM_WIDTH, 3)))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(BatchNormalization())
     model.add(Conv2D(16, (3, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(BatchNormalization())
     model.add(Flatten())
-    model.add(Dense(1024, activation='relu'))
+    model.add(Dense(512, activation='relu'))
     model.add(BatchNormalization())
     model.add(Dense(80, activation='softmax'))
 
