4c05c122cf9c83458427d25cb72770fb1dfe46c2
zijiao <1129090915@qq.com>
2017-11-22 14:34:10 +0800

    add the logic with getting best weights

--- utils.py
+++ utils.py
@@ -18,31 +18,34 @@ def ensure_dir(dir):
         os.makedirs(dir)
 
 
-def get_best_weights(path_weights, mode='acc'):
+def get_best_weights(path_weights, mode='acc', postfix='.h5'):
     if not os.path.isdir(path_weights):
         return None
     sub_files = os.listdir(path_weights)
     if not sub_files:
         return None
-    try:
-        weights_value = [file.replace('.h5', '').split('-')[1:] for file in sub_files if
-                         file.endswith('.h5') and file.__contains__('-')]
-        key_filename = 'filename'
-        kw = ['loss', 'acc']
-        weights_info = []
-        for filename, value in zip(sub_files, weights_value):
-            item = dict((k, float(v)) for k, v in zip(kw, value))
-            item[key_filename] = filename
-            weights_info.append(item)
-        if mode not in kw:
-            mode = 'acc'
-        if mode == 'loss':
-            weights_info = sorted(weights_info, key=lambda x: x['loss'])
-        elif mode == 'acc':
-            weights_info = sorted(weights_info, key=lambda x: x['acc'], reverse=True)
-        target = weights_info[0][key_filename]
-        print('The best weights is %s, sorted by %s.' % (target, mode))
-    except:
-        target = sub_files[0]
-        print('Parse best weights failure, choose first file %s.' % target)
+    target = sub_files[0]
+    sub_files_with_metric = filter(lambda f: f.endswith(postfix) and f.__contains__('-'), sub_files)
+    if sub_files_with_metric:
+        try:
+            weights_value = [file.replace(postfix, '').split('-')[-2:] for file in sub_files_with_metric]
+            key_filename = 'filename'
+            kw = ['loss', 'acc']
+            weights_info = []
+            for filename, value in zip(sub_files_with_metric, weights_value):
+                item = dict((k, float(v)) for k, v in zip(kw, value))
+                item[key_filename] = filename
+                weights_info.append(item)
+            if mode not in kw:
+                mode = 'acc'
+            if mode == 'loss':
+                weights_info = sorted(weights_info, key=lambda x: x['loss'])
+            elif mode == 'acc':
+                weights_info = sorted(weights_info, key=lambda x: x['acc'], reverse=True)
+            target = weights_info[0][key_filename]
+            print('The best weights is %s, sorted by %s.' % (target, mode))
+        except:
+            print('Parse best weights failure, choose first file %s.' % target)
+    else:
+        print('No weights with metric found, choose first file %s.' % target)
     return os.path.join(path_weights, target)
