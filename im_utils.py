259988f57b051f6c3fcc0f46c27988d419dd9486
puke3615 <1129090915@qq.com>
2017-11-23 20:21:50 +0800

    add custom image generator

--- im_utils.py
+++ im_utils.py
@@ -16,45 +16,66 @@ def im2array(files, target_size, mode='test', grayscale=False):
         else:
             if img.mode != 'RGB':
                 img = img.convert('RGB')
-        size = target_size
-        w, h = img.size
-        l, t, r, b = 0, 0, w, h
-        offset = abs(w - h)
         if mode == 'train':
-            # output 1 samples
-            if w > h:
-                l = random.randint(0, offset)
-                r = l + h
-            else:
-                t = random.randint(0, offset)
-                b = t + w
-            img = img.crop((l, t, r, b)).resize([size, size], Image.BOX)
+            img = random_crop(img, target_size)
             outputs.append(np.asarray(img))
         elif mode == 'test':
-            # output 6 samples
-            images = []
-            if w > h:
-                # left
-                images.append(img.crop((0, 0, h, h)))
-
-                # center
-                images.append(img.crop(((w - h) / 2, 0, (w + h) / 2, h)))
-
-                # right
-                images.append(img.crop((w - h, 0, w, h)))
-            else:
-                # top
-                images.append(img.crop((0, 0, w, w)))
-
-                # center
-                images.append(img.crop(((h - w) / 2, 0, (h + w) / 2, w)))
-
-                # bottom
-                images.append(img.crop((h - w, 0, h, w)))
+            images = boundary_crop(img, target_size)
             for image in images:
-                image = image.resize([size, size])
                 outputs.append(np.asarray(image))
-                outputs.append(np.asarray(image.transpose(Image.FLIP_LEFT_RIGHT)))
             patch = len(images)
     outputs = np.array(outputs)
     return outputs, patch
+
+
+def boundary_crop(img, target_size):
+    # output 6 samples
+    size = target_size
+    w, h = img.size
+    images = []
+    if w > h:
+        # left
+        images.append(img.crop((0, 0, h, h)).resize([size, size]))
+
+        # center
+        images.append(img.crop(((w - h) / 2, 0, (w + h) / 2, h)).resize([size, size]))
+
+        # right
+        images.append(img.crop((w - h, 0, w, h)).resize([size, size]))
+    else:
+        # top
+        images.append(img.crop((0, 0, w, w)).resize([size, size]))
+
+        # center
+        images.append(img.crop(((h - w) / 2, 0, (h + w) / 2, w)).resize([size, size]))
+
+        # bottom
+        images.append(img.crop((h - w, 0, h, w)).resize([size, size]))
+    images += [image.transpose(Image.FLIP_LEFT_RIGHT) for image in images]
+    return images
+
+
+def center_crop(img, target_size):
+    # output 6 samples
+    size = target_size
+    w, h = img.size
+    if w > h:
+        return img.crop(((w - h) / 2, 0, (w + h) / 2, h)).resize([size, size])
+    else:
+        return img.crop(((h - w) / 2, 0, (h + w) / 2, w)).resize([size, size])
+
+
+def random_crop(img, target_size):
+    # output 1 samples
+    size = target_size
+    w, h = img.size
+    l, t, r, b = 0, 0, w, h
+    offset = abs(w - h)
+    if w > h:
+        l = random.randint(0, offset)
+        r = l + h
+    else:
+        t = random.randint(0, offset)
+        b = t + w
+    img = img.crop((l, t, r, b)).resize([size, size], Image.BOX)
+    return img
