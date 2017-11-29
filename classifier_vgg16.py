6d7b733c60312c3a4ed8331846ab7deb46d198c3
zijiao <1129090915@qq.com>
2017-11-29 17:02:02 +0800

    add vgg_little modelg

--- classifier_vgg16.py
+++ classifier_vgg16.py
@@ -1,3 +1,6 @@
+import keras.optimizers
+
+import generator
 from classifier_base import BaseClassifier
 from keras.applications import *
 from keras.layers import *
@@ -25,7 +28,29 @@ class VGG16Classifier(BaseClassifier):
         model = Model(inputs=model_vgg16.inputs, outputs=x)
         return model
 
+    def image_generator(self, train=True):
+        def wrap(value):
+            return float(train) and value
+
+        return generator.ImageDataGenerator(
+            contrast_stretching=train,
+            channel_shift_range=wrap(25.5),
+            rotation_range=wrap(6.),
+            width_shift_range=wrap(0.05),
+            height_shift_range=wrap(0.05),
+            shear_range=wrap(0.05),
+            zoom_range=wrap(0.05),
+            horizontal_flip=train,
+            fill_mode='constant',
+            preprocessing_function=generator.scene_preprocess_input,
+        )
+
+    def data_generator(self, path_image, train=True):
+        generator = BaseClassifier.data_generator(self, path_image, train)
+        generator.crop_mode = None
+        return generator
+
 
 if __name__ == '__main__':
-    classifier = VGG16Classifier('vgg16', lr=2e-3)
+    classifier = VGG16Classifier('vgg16_little', optimizer=keras.optimizers.Adam())
     classifier.train()
